f650ee5a300e569234ea6f3d0594812a5b4eb9a6
jhoynerk <jhoynerk@softwarecriollo.com>
2015-04-19 18:01:43 -0430

    Cargar archivo files/locales.txt en select

--- index.php
+++ index.php
@@ -19,12 +19,41 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
+
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <div class="header clearfix">
+        <h3 class="text-muted">Project name</h3>
+      </div>
 
+      <div class="jumbotron">
+        <h1>Locales</h1>
+        <form class="form-horizontal" action="">
+          <div class="form-group">
+            <div class="col-sm-12">
+              <select class="form-control" name="local" id="local">
+              <option value="">Seleccione un local</option>
+          <?php $myfile = fopen("files/locales.txt", "r") or die("Tenemos problemas al abrir el archivo locales.txt!");
+                while (($line = fgets($myfile)) !== false) : ?>
+                  <option value="<?php echo $line ?>"><?php echo $line ?></option>
+          <?php endwhile;
+                fclose($myfile); ?>
+               </select>
+            </div>
+          </div>
 
+          <div class="form-group">
+            <div class="col-sm-12">
+              <button type="submit" class="btn btn-success">Buscar</button>
+            </div>
+          </div>
+        </form>
+      </div>
 
-
+      <footer class="footer">
+        <p>© Company 2015</p>
+      </footer>
+    </div> <!-- /container -->
     <!-- Script -->
       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
       <script src="js/jquery.min.js"></script>
