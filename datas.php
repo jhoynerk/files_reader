b821fcffff3206e08ea85e4ec52c6832b4df870b
jhoynerk <jhoynerk@softwarecriollo.com>
2015-05-25 06:32:36 -0430

    html para el informe de datos.

---
+++ datas.php
@@ -0,0 +1,181 @@
+<?php include_once('base/config.php'); estaLogin(); ?>
+<?php include_once('vistas/head.php'); ?>
+  <body>
+    <div class="container">
+      <?php include_once('vistas/cabecera.php');
+        $input = $_REQUEST;
+        $fichero = path($input['local']);
+        $datos = open_file($fichero);
+        if(!$datos['success']):
+          echo $datos['msg'];
+        endif;
+      ?>
+      <div class="header clearfix">
+        <h3 class="text-muted">Lector de Archivos</h3>
+      </div>
+      <div class="jumbotron">
+        <a class="btn btn-default" href="local.php">Regresar</a>
+        <h1>Local <small><?php echo $input['local'].' '; echo $input['fecha'] ?></small> </h1>
+        <div class="form-horizontal">
+          <div class="form-group">
+            <label for="maquina" class="col-sm-2 control-label">Máquina</label>
+            <div class="col-sm-4">
+              <select class="form-control" id="select-maquina" required>
+              <option value="">Seleccione una maquina</option>
+              <?php
+                if($datos['success']):
+                  foreach($datos['data'] as $valor):
+                    echo "<option value='".$valor[$index['numeroMaquina']]."'>".$valor[$index['numeroMaquina']]."</option>";
+                  endforeach;
+                endif;
+              ?>
+              </select>
+            </div>
+            <label for="dias_recaudados" class="col-sm-2 control-label">Días a Recaudar</label>
+            <div class="col-sm-4">
+              <input class="form-control" type="number" min="0" value="0" name="dias_recaudados" id="dias_recaudados" required>
+            </div>
+          </div>
+        </div>
+          <br><br>
+        <form class="form-horizontal" action="json/generar_archivo.php" method="POST" id="save_file">
+          <input type="hidden" name="local" value="<?php echo $input['local'] ?>">
+          <input type="hidden" name="fecha" value="<?php echo $input['fecha'] ?>">
+          <input type="hidden" name="maquina" id="maquina">
+          <div id="cargar_planilla">
+            <div class="col-sm-2" id="fecha"><b>Fecha: </b></div>
+            <div class="col-sm-6" id="titulo1"><b>Titulo 1</b></div>
+            <div class="col-sm-4" id="titulo3"><b>Titulo 3</b></div>
+            <div class="col-sm-6 col-sm-offset-2" id="titulo2"><b>Titulo 2</b></div>
+            <div class="col-sm-4" id="titulo4"><b>Titulo 4</b></div>
+            <div class="col-sm-4" id="maquina-nro"><b>Maquina nro: </b></div>
+            <div class="col-sm-4" id="modelo"><b>Modelo: </b></div>
+            <div class="col-sm-4" id="denom"><b>Denom: </b></div>
+            <div class="col-sm-12 col-md-8">
+              <div class="table-responsive">
+                <table class="table">
+                  <tr class="titulo">
+                    <th colspan="3">Contadores Mecanicos:</th>
+                    <th colspan="3">Contadores Electronicos:</th>
+                  </tr>
+                  <tr>
+                    <td class="col-sm-1"></td>
+                    <td class="col-sm-1">ENTRADA</td>
+                    <td class="col-sm-1">SALIDA</td>
+                    <td class="col-sm-1"></td>
+                    <td class="col-sm-1">ENTRADA</td>
+                    <td class="col-sm-1">SALIDA</td>
+                  </tr>
+                  <tr>
+                    <td class="col-sm-1">Finales: </td>
+                    <td class="col-sm-1"><input type="text" id="mecanicoFinalE" name="mecanicoFinalE" class="form-control inputs"></td>
+                    <td class="col-sm-1"><input type="text" id="mecanicoFinalS" name="mecanicoFinalS" class="form-control inputs"></td>
+                    <td class="col-sm-1">Finales: </td>
+                    <td class="col-sm-1"><input type="text" id="electronicoFinalE" name="electronicoFinalE" class="form-control inputs"></td>
+                    <td class="col-sm-1"><input type="text" id="electronicoFinalS" name="electronicoFinalS" class="form-control inputs"></td>
+                  </tr>
+                  <tr>
+                    <td class="col-sm-1">Iniciales: </td>
+                    <td id="mecanicoInicialE">----</td>
+                    <td id="mecanicoInicialS">----</td>
+                    <td class="col-sm-1">Iniciales: </td>
+                    <td id="electronicoInicialE">----</td>
+                    <td id="electronicoInicialS">----</td>
+                  </tr>
+                  <tr>
+                    <td class="col-sm-1">Total: </td>
+                    <td id="mecanicoTotalE">----</td>
+                    <td id="mecanicoTotalS">----</td>
+                    <td class="col-sm-1">Total: </td>
+                    <td id="electronicoTotalE">----</td>
+                    <td id="electronicoTotalS">----</td>
+                  </tr>
+                  <tr>
+                    <td class="col-sm-1">Utilidad: </td>
+                    <td class="col-sm-1">----</td>
+                    <td class="col-sm-1"></td>
+                    <td class="col-sm-1">Utilidad: </td>
+                    <td class="col-sm-1">----</td>
+                    <td class="col-sm-1"></td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+              <div class="table-responsive">
+                <table class="table">
+                  <tr>
+                    <td>Colectado:</td>
+                    <td id="colectado">-----</td>
+                  </tr>
+                  <tr>
+                    <td>Premios:</td>
+                    <td id="premios">-----</td>
+                  </tr>
+                  <tr>
+                    <td>Devol:</td>
+                    <td id="devolucion">-----</td>
+                  </tr>
+                  <tr>
+                    <td>R. Bruta:</td>
+                    <td id="rBruta">-----</td>
+                  </tr>
+                  <tr>
+                    <td>J.C.J:</td>
+                    <td id="jcj">-----</td>
+                  </tr>
+                  <tr>
+                    <td>Dividendo:</td>
+                    <td id="dividendo">-----</td>
+                  </tr>
+                  <tr>
+                    <td>50%:</td>
+                    <td id="porcentaje1">-----</td>
+                  </tr>
+                  <tr>
+                    <td>50%:</td>
+                    <td id="porcentaje2">-----</td>
+                  </tr>
+                  <tr>
+                    <td>Cliente:</td>
+                    <td id="cliente">-----</td>
+                  </tr>
+                  <tr>
+                    <td>Empresa:</td>
+                    <td id="empresa">-----</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="col-sm-12">
+              <div class="form-group">
+              <label for="observaciones" class="control-label">Observaciones: </label>
+              <textarea class="form-control" rows="3" name="observaciones" id="observaciones"></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-10">
+              <button type="submit" class="btn btn-success">Guardar</button>
+            </div>
+          </div>
+        </form>
+        <div class="clearfix"></div>
+      </div>
+
+      <?php include_once('vistas/footer.php'); ?>
+    </div> <!-- /container -->
+    <!-- Script -->
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="js/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+      <script>
+        var fecha = "<?php echo $input['fecha'] ?>"
+        var local = "<?php echo $input['local'] ?>"
+      </script>
+      <script src="js/form_file.js"></script>
+      <script src="js/operaciones.js"></script>
+    <!-- /Script -->
+  </body>
+</html>
