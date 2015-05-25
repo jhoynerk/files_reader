0d99daf1550af962865572468b360dd034be28c4
jhoynerk <jhoynerk@softwarecriollo.com>
2015-05-25 06:33:38 -0430

    vistas para incluir

---
+++ vistas/cabecera.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+
+      <a class="navbar-brand" href="#"></a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="logout.php">Cerrar sessión</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div><!--/.container-fluid -->
+</nav>
